1ad28b7c8dfbefe8e6a36858ed91129679397101
a45085 <89801549+a45085@users.noreply.github.com>
2021-08-30 23:36:42 +0100

    Delete 03_Implementacao/DCS-master/DCS-master/DCS-SYS directory

--- 03_Implementacao/DCS-master/DCS-master/DCS-SYS/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import navReducer from "./slices/navSlice";
-
-export const store = configureStore({
-  reducer: {
-    //reducer faz setup de toda a store
-    nav: navReducer,
-  },
-});
